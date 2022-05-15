4163658772cef4c7b66bb14ab6681cb604af1b8b
Md. Sariful Islam <ssahed65@gmail.com>
2022-05-15 10:25:11 +0600

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
